6bf49a5443c6c11e06585b2708634c13d12eaffd
鲁辰喜 <luchenxi.axzz@bytedance.com>
2021-10-11 17:19:28 +0800

    修复

--- Demo/YYKitDemo/YYWebImageExample.m
+++ Demo/YYKitDemo/YYWebImageExample.m
@@ -136,29 +136,29 @@
         @"http://i.imgur.com/WXJaqof.gif",
         
         // animated gif: https://dribbble.com/markpear
-        @"https://d13yacurqjgara.cloudfront.net/users/345826/screenshots/1780193/dots18.gif",
-        @"https://d13yacurqjgara.cloudfront.net/users/345826/screenshots/1809343/dots17.1.gif",
-        @"https://d13yacurqjgara.cloudfront.net/users/345826/screenshots/1845612/dots22.gif",
-        @"https://d13yacurqjgara.cloudfront.net/users/345826/screenshots/1820014/big-hero-6.gif",
-        @"https://d13yacurqjgara.cloudfront.net/users/345826/screenshots/1819006/dots11.0.gif",
-        @"https://d13yacurqjgara.cloudfront.net/users/345826/screenshots/1799885/dots21.gif",
+        @"https://cdn.dribbble.com/users/345826/screenshots/1780193/dots18.gif",
+        @"https://cdn.dribbble.com/users/345826/screenshots/1809343/dots17.1.gif",
+        @"https://cdn.dribbble.com/users/345826/screenshots/1845612/dots22.gif",
+        @"https://cdn.dribbble.com/users/345826/screenshots/1820014/big-hero-6.gif",
+        @"https://cdn.dribbble.com/users/345826/screenshots/1819006/dots11.0.gif",
+        @"https://cdn.dribbble.com/users/345826/screenshots/1799885/dots21.gif",
         
         // animaged gif: https://dribbble.com/jonadinges
-        @"https://d13yacurqjgara.cloudfront.net/users/288987/screenshots/2025999/batman-beyond-the-rain.gif",
-        @"https://d13yacurqjgara.cloudfront.net/users/288987/screenshots/1855350/r_nin.gif",
-        @"https://d13yacurqjgara.cloudfront.net/users/288987/screenshots/1963497/way-back-home.gif",
-        @"https://d13yacurqjgara.cloudfront.net/users/288987/screenshots/1913272/depressed-slurp-cycle.gif",
+        @"https://cdn.dribbble.com/users/288987/screenshots/2025999/batman-beyond-the-rain.gif",
+        @"https://cdn.dribbble.com/users/288987/screenshots/1855350/r_nin.gif",
+        @"https://cdn.dribbble.com/users/288987/screenshots/1963497/way-back-home.gif",
+        @"https://cdn.dribbble.com/users/288987/screenshots/1913272/depressed-slurp-cycle.gif",
         
         // jpg: https://dribbble.com/snootyfox
-        @"https://d13yacurqjgara.cloudfront.net/users/26059/screenshots/2047158/beerhenge.jpg",
-        @"https://d13yacurqjgara.cloudfront.net/users/26059/screenshots/2016158/avalanche.jpg",
-        @"https://d13yacurqjgara.cloudfront.net/users/26059/screenshots/1839353/pilsner.jpg",
-        @"https://d13yacurqjgara.cloudfront.net/users/26059/screenshots/1833469/porter.jpg",
-        @"https://d13yacurqjgara.cloudfront.net/users/26059/screenshots/1521183/farmers.jpg",
-        @"https://d13yacurqjgara.cloudfront.net/users/26059/screenshots/1391053/tents.jpg",
-        @"https://d13yacurqjgara.cloudfront.net/users/26059/screenshots/1399501/imperial_beer.jpg",
-        @"https://d13yacurqjgara.cloudfront.net/users/26059/screenshots/1488711/fishin.jpg",
-        @"https://d13yacurqjgara.cloudfront.net/users/26059/screenshots/1466318/getaway.jpg",
+        @"https://cdn.dribbble.com/users/26059/screenshots/2047158/beerhenge.jpg",
+        @"https://cdn.dribbble.com/users/26059/screenshots/2016158/avalanche.jpg",
+        @"https://cdn.dribbble.com/users/26059/screenshots/1839353/pilsner.jpg",
+        @"https://cdn.dribbble.com/users/26059/screenshots/1833469/porter.jpg",
+        @"https://cdn.dribbble.com/users/26059/screenshots/1521183/farmers.jpg",
+        @"https://cdn.dribbble.com/users/26059/screenshots/1391053/tents.jpg",
+        @"https://cdn.dribbble.com/users/26059/screenshots/1399501/imperial_beer.jpg",
+        @"https://cdn.dribbble.com/users/26059/screenshots/1488711/fishin.jpg",
+        @"https://cdn.dribbble.com/users/26059/screenshots/1466318/getaway.jpg",
         
         // animated webp and apng: http://littlesvr.ca/apng/gif_apng_webp.html
         @"http://littlesvr.ca/apng/images/BladeRunner.png",
